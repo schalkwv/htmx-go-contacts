f3e6adf4aff397c775bd78b6fc6649c5eb90fb78
schalk <sv@easybooking.at>
2024-01-16 06:35:57 +0100

    view form working

--- go.mod
+++ go.mod
@@ -3,8 +3,15 @@ module github.com/schalkwv/htmx-go-contacts
 go 1.21.3
 
 require (
-	github.com/labstack/echo/v4 v4.11.4 // indirect
+	github.com/go-playground/validator v9.31.0+incompatible
+	github.com/labstack/echo/v4 v4.11.4
+)
+
+require (
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
+	github.com/leodido/go-urn v1.2.4 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
@@ -13,4 +20,5 @@ require (
 	golang.org/x/net v0.19.0 // indirect
 	golang.org/x/sys v0.15.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
+	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
 )
--- server.go
+++ server.go
@@ -3,16 +3,18 @@ package main
 import (
 	"html/template"
 	"net/http"
+	"strconv"
 
+	"github.com/go-playground/validator"
 	"github.com/labstack/echo/v4"
 )
 
 type contact struct {
 	ID    int    `json:"id" form:"ID"`
-	First string `json:"first" form:"first_name"`
-	Last  string `json:"last" form:"last_name"`
-	Phone string `json:"phone" form:"phone"`
-	Email string `json:"email" form:"email"`
+	First string `json:"first" form:"first_name" validate:"required"`
+	Last  string `json:"last" form:"last_name" validate:"required"`
+	Phone string `json:"phone" form:"phone" validate:"required"`
+	Email string `json:"email" form:"email" validate:"required"`
 }
 
 var Contacts []contact
@@ -45,12 +47,38 @@ func getNewContactForm(c echo.Context) error {
 
 func createContact(c echo.Context) error {
 	var newContact contact
-	err := c.Bind(&newContact)
+	err := BindAndValidate(c, &newContact)
+	if err != nil {
+		tmpl := template.Must(template.New("").ParseGlob("templates/*.gohtml"))
+
+		err := tmpl.ExecuteTemplate(c.Response().Writer, "NewContactPage", newContact)
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, err.Error())
+		}
+	} else {
+		Contacts = append(Contacts, newContact)
+		return c.Redirect(http.StatusMovedPermanently, "/contacts")
+	}
+	return nil
+}
+
+func getViewContactForm(c echo.Context) error {
+	id := c.Param("id")
+	var contact contact
+	for _, c := range Contacts {
+		if strconv.Itoa(c.ID) == id {
+			contact = c
+			break
+		}
+	}
+
+	tmpl := template.Must(template.New("").ParseGlob("templates/*.gohtml"))
+
+	err := tmpl.ExecuteTemplate(c.Response().Writer, "ViewContactPage", contact)
 	if err != nil {
 		return c.JSON(http.StatusInternalServerError, err.Error())
 	}
-	Contacts = append(Contacts, newContact)
-	return c.Redirect(http.StatusMovedPermanently, "/contacts")
+	return nil
 }
 
 func main() {
@@ -61,5 +89,30 @@ func main() {
 	e.GET("/contacts", getContacts)
 	e.GET("/contacts/new", getNewContactForm)
 	e.POST("/contacts/new", createContact)
+	e.GET("/contacts/:id", getViewContactForm)
+
+	e.Validator = &Validator{validator: validator.New()}
+
 	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func BindAndValidate(c echo.Context, req interface{}) error {
+	if err := c.Bind(req); err != nil {
+		return err
+	}
+	if err := c.Validate(req); err != nil {
+		return err
+	}
+	return nil
+}
+
+// Validator is a custom validator for Echo.
+type Validator struct {
+	validator *validator.Validate
+}
+
+// Validate validates the request according to the required tags.
+// Returns HTTPError if the required parameter is missing in the request.
+func (v *Validator) Validate(i interface{}) error {
+	return v.validator.Struct(i)
 }
